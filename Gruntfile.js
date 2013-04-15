1e962b6359a7640de93ce585c5edbc344aa4e138
Andreas Hölzl <andy.woodly@googlemail.com>
2013-04-15 15:03:55 +0200

    added grunt build file

---
+++ Gruntfile.js
@@ -0,0 +1,70 @@
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+            pkg: grunt.file.readJSON('package.json'),
+
+            requirejs: {
+                compile: {
+                    options: {
+                        // main file to start to look for its dependencies.
+                        name          : 'Dolphin',
+                        baseUrl       : "src/",
+                        mainConfigFile: "config.js",
+                        optimize      : "uglify",
+                        out           : "build/<%= pkg.bundle %>.min.js"
+                    }
+                }
+            },
+
+            buster: {
+                test  : {
+                    config     : 'test/buster.js',
+//                    reporter   : 'xml',
+                    'log-level': 'debug'
+                },
+                server: {
+                    port: 1111
+                }
+            },
+
+            jshint: {
+                options: {
+                    curly  : true,
+                    eqeqeq : true,
+                    immed  : true,
+                    latedef: true,
+                    newcap : true,
+                    noarg  : true,
+                    sub    : true,
+                    undef  : true,
+                    boss   : true,
+                    eqnull : true,
+                    node   : true
+                },
+                globals: {
+                    exports: true,
+                    $      : true,
+                }
+            },
+
+            clean: {
+                build: [
+                    'build/'
+                ]
+            }
+
+        }
+    );
+
+    grunt.loadNpmTasks('grunt-contrib');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-buster');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+
+    // Default task.
+    grunt.registerTask('test', 'buster');
+    grunt.registerTask('default', 'build'.split(' '));
+    grunt.registerTask('build', 'clean requirejs'.split(' '));
+};
